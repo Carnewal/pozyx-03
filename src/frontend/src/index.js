91c0b234add94c024de5904db77329ecd06a0076
Brecht Carnewal <bradster@live.be>
2017-03-08 16:27:24 +0100

    Added react-material-admin-template copyright notice

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/default */
-
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
@@ -8,6 +6,17 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import './styles.scss'
 import 'font-awesome/css/font-awesome.css'
 import 'flexboxgrid/css/flexboxgrid.css'
+
+/**
+ *  Copyright Notices:
+ *
+ *  © react-material-admin-template by rafaelhz (MIT)
+ *  https://github.com/rafaelhz/react-material-admin-template
+ *
+ *
+ */
+
+
 
 injectTapEventPlugin()
 
