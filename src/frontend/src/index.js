2bad11b9d4e2153dc1280216dd29d54618764e9a
Brecht Carnewal <bradster@live.be>
2017-03-08 14:53:01 +0100

    Swapped current frontend code with react-material-admin-template code.
    This introduced a lot of new dependencies like flexboxgrid, material-ui and various loaders for webpack.

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,13 +1,16 @@
+/* eslint-disable import/default */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { configureStore } from './store/configureStore';
-import Root from './containers/Root';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './styles.scss';
+import 'font-awesome/css/font-awesome.css';
+import 'flexboxgrid/css/flexboxgrid.css';
 
-import './styles/main.scss';
+injectTapEventPlugin();
 
-const store = configureStore();
-
-ReactDOM.render(
-  <Root store={store} />,
-  document.getElementById('root')
+render(
+    <Router routes={routes} history={browserHistory} />, document.getElementById('root')
 );
--- src/webpack.config.server.js
+++ src/webpack.config.server.js
@@ -29,7 +29,8 @@ module.exports = {
         query: {
           presets: [
             'react',
-            'es2015'
+            'es2015',
+            'stage-3'
           ]
         },
       }, {
