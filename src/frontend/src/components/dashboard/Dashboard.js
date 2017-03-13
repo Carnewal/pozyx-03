4e5f85b242ef6c5780fb78ea2230613e601d65b8
Brecht Carnewal <Brecht Carnewal>
2017-03-13 17:15:40 +0100

    Reintroduced style.scss (was failing for me)

--- src/frontend/src/components/dashboard/Dashboard.js
+++ src/frontend/src/components/dashboard/Dashboard.js
@@ -1,15 +1,14 @@
 import React, {PropTypes} from 'react'
 import UploadPlanArea from '../../containers/UploadPlanArea'
 import Map from '../../containers/MapContainer'
-import InfoBox from 'frontend/components/dashboard/InfoBox';
-import RecentlyProducts from 'frontend/components/dashboard/RecentlyProducts';
+import InfoBox from 'frontend/components/dashboard/InfoBox'
+import RecentlyProducts from 'frontend/components/dashboard/RecentlyProducts'
+import PageBase from 'frontend/components/layout/PageBase'
 
-import {cyan600, pink600, purple600, orange600} from 'material-ui/styles/colors';
+import {cyan600, purple600} from 'material-ui/styles/colors'
 
-import TagIcon from 'material-ui/svg-icons/maps/my-location';
-import AnchorIcon from 'material-ui/svg-icons/action/perm-scan-wifi';
-
-import globalStyles from 'frontend/styles';
+import TagIcon from 'material-ui/svg-icons/maps/my-location'
+import AnchorIcon from 'material-ui/svg-icons/action/perm-scan-wifi'
 
 
 export default class Dashboard extends React.Component {
@@ -19,27 +18,27 @@ export default class Dashboard extends React.Component {
 
   render() {
 
-    const {currentMap, floorPlan, tagsAmount, anchorsAmount} = this.props
+    const {floorPlan, tagsAmount, anchorsAmount} = this.props
     return (
-      <div>
-      <h3 style={globalStyles.navigation}>Application / Dashboard</h3>
-      <h1>Map {currentMap}</h1>
-      <div className="row">
+      <PageBase title='Dashboard'
+        navigation='Map / Dashboard'>
+        <br/>
+      <div className='row'>
 
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+        <div className='col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 '>
           <InfoBox Icon={TagIcon}
                    color={purple600}
-                   title="Tags"
+                   title='Tags'
                    value={tagsAmount}
           />
         </div>
 
 
-        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
+        <div className='col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 '>
           <InfoBox Icon={AnchorIcon}
                    color={cyan600}
-                   title="Anchors"
+                   title='Anchors'
                    value={anchorsAmount}
           />
         </div>
@@ -49,15 +48,15 @@ export default class Dashboard extends React.Component {
 
       <br/>
 
-      <div className="row">
-      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 m-b-15 ">
-        {floorPlan == '' ? <UploadPlanArea/> : <Map />}
+      <div className='row'>
+        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 m-b-15 '>
+          {floorPlan == '' ? <UploadPlanArea/> : <Map />}
+
+        </div>
 
       </div>
 
-      </div>
-    </div>
-
+    </PageBase>
 
 
     )
