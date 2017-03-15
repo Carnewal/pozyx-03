53f783a00908e4497906877058358d0da2364bfb
Rutger Benoot <rutgerbenoot@hotmail.com>
2017-03-15 21:20:45 +0100

    poort terug toegevoegd aan de saga + gefixed dat devtools noodzakelijk zijn

--- src/frontend/src/sagas/WebsocketSaga.js
+++ src/frontend/src/sagas/WebsocketSaga.js
@@ -5,7 +5,7 @@ import { showPositions, SHOW_POSITIONS } from 'frontend/actions/TagActions'
 
 function initWebsocket() {
   return eventChannel(emitter => {
-    const primus = new Primus('http://'+window.location.hostname, {})
+    const primus = new Primus('http://'+window.location.hostname + window.location.port?":"+window.location.port:"", {})
 
     primus.on('data', (data) => {
       switch (data.action) {
--- src/frontend/src/store/index.js
+++ src/frontend/src/store/index.js
@@ -9,18 +9,18 @@ import uploadFloorplanSaga from 'frontend/sagas/UploadFloorplanSaga'
 const sagaMiddleware = createSagaMiddleware()
 const logger = createLogger()
 const finalCreateStore = compose(
-  applyMiddleware(
-    logger,
-    sagaMiddleware
-  ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)(createStore)
+    applyMiddleware(
+      logger,
+      sagaMiddleware
+    )
+  )(createStore)
 const store = finalCreateStore(
   combineReducers({
     ...reducers,
     routing: routerReducer
   }),
-  {} // Initial State
+  {}, // Initial State
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 sagaMiddleware.run(websocketSaga)
 sagaMiddleware.run(uploadFloorplanSaga)
