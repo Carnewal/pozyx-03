a298a28ca92a710aa876e12fa4aa295931316545
Pieter-Jan Vandenberghe <pieterjan.vandenberghe@ugent.be>
2017-04-09 16:13:47 +0200

    #22 Integreer triggers met realtime

--- src/server/src/middleware/trigger/notifier.js
+++ src/server/src/middleware/trigger/notifier.js
@@ -1,9 +1,42 @@
 import State from './state'
+import Model from '../model'
 
 class Notifier {
   prevState = new State()
   currState = new State()
   triggers = new Map()
+
+  initState = (mapId) => { //TODO Anchor/zone implementation
+    Model.Tag.findAll({
+      where: {
+        mapId: mapId
+      },
+      include: [{model: Model.Label, through: Model.TagLabel, as: 'labels'}]
+    }).then((tags) => {
+      tags.forEach((tag) => {
+        this.prevState.updateTag(tag)
+      })
+      this.currState = this.prevState
+    })
+  }
+
+  updateState = (changes) => {
+    this.prevState = this.currState
+    changes.tags.forEach((tag) => {
+      this.currState.updateTag(tag)
+    })
+    changes.zones.forEach((tag) => {
+      this.currState.updateZone(tag)
+    })
+    changes.anchors.forEach((tag) => {
+      this.currState.updateAnchor(tag)
+    })
+    this.check()
+  }
+
+  initTriggers = (mapId) => {
+    //TODO get existing triggers from db and add to triggers map
+  }
 
   addTrigger = (trigger) => {
     this.triggers.set(trigger.id, trigger)
--- src/server/src/realtime.js
+++ src/server/src/realtime.js
@@ -81,6 +81,7 @@ const mgr = new Manager(
 const realtime = (server) => {
   const primus = new Primus(server, {})
   const notifier = new Notifier() //TODO notifier integreren in live data
+  notifier.initState(1) //INIT with map 1
 
   primus.on('connection', () => {
     console.log('client connected')
@@ -94,6 +95,13 @@ const realtime = (server) => {
     mgr.tags.forEach(function(tag) {
       const tagPosition = {
         tagId: tag.tagId,
+        id: tag.tagId,
+        position: {
+          x: tag.position.x,
+          y: tag.position.y,
+          z: tag.position.z,
+          timestamp: new Date().toISOString()
+        },
         x: tag.position.x,
         y: tag.position.y,
         z: tag.position.z,
@@ -102,6 +110,7 @@ const realtime = (server) => {
       tagPositions.push(tagPosition)
     })
 
+    notifier.updateState({tags: tagPositions})
     primus.write({action: 'SHOW_POSITIONS', positions: tagPositions})
   }, interval)
 }
