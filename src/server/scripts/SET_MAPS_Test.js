4755c6033a8c2924720b0bb3cb09290daf30c15c
tdorchai <Tom.Dorchain@UGent.be>
2017-04-24 22:14:23 +0200

    Add files via upload

---
+++ src/server/scripts/SET_MAPS_Test.js
@@ -0,0 +1,62 @@
+import * as types from '../src/frontend/src/actions/MapActions'
+import reducer from '../src/frontend/src/reducers/app'
+
+describe('app reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle SET_CURRENTMAP', () => {
+    expect(
+      reducer([], {
+        type: types.SET_CURRENTMAP,
+        text: 'Run the tests'
+      })
+    ).toEqual(
+      [
+        {
+          text: 'Run the tests',
+          completed: false,
+          id: 0
+        }
+      ]
+    )
+
+    expect(
+      reducer(
+        [
+          {
+            text: 'Use Redux',
+            completed: false,
+            id: 0
+          }
+        ],
+        {
+          type: types.SET_CURRENTMAP,
+          text: 'Run the tests'
+        }
+      )
+    ).toEqual(
+      [
+        {
+          text: 'Run the tests',
+          completed: false,
+          id: 1
+        },
+        {
+          text: 'Use Redux',
+          completed: false,
+          id: 0
+        }
+      ]
+    )
+  })
+})
