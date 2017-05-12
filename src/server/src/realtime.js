ac183894097646828d63fc32fc9f03315d2268d4
rdheygere <remy.dheygere@ugent.be>
2017-05-12 14:56:01 +0200

    Added tag mocking for testing

--- src/server/src/realtime.js
+++ src/server/src/realtime.js
@@ -6,14 +6,16 @@ const maxdistance = {x: 225, y: 150, z: 3} // meter
 
 const interval = 1000 / 30
 
-const BatteryModes = {   CHARGE: {value : 0.1},
-                         DRAIN: {value: -0.1},
-                         FREEZE: {value: 0} 
+const BatteryModes = {   CHARGE:  {value: 0.001},
+                         DRAIN:   {value: -0.001},
+                         FREEZE:  {value: 0} 
 }
 
 class Tag {
-  constructor(tagId, speed) {
-    this.tagId = tagId
+  constructor(modelTag, speed) {
+    this.id = modelTag.id
+    this.name = modelTag.name
+    this.labels = modelTag.labels
     this.timestamp = new Date().toISOString()
     this.position = {
       x: Math.random() * maxdistance.x,
@@ -22,14 +24,15 @@ class Tag {
     }
     const x = 1 - Math.random() * 2
     const y = Math.random() > 0.5 ? Math.sqrt(1.0 - x * x) : -Math.sqrt(1.0 - x * x)
-    this.direction = { // should be unitvector
+    this.direction = {                      // should be unitvector
       x: x,
       y: y,
       z: 0
     }
     this.speed = speed
-    this.batteryLevel = 1.0
-    this.batteryMode = BatteryModes.FREEZE
+    this.originalSpeed = speed
+    this.batteryLevel = 1.0                 //hardcoded
+    this.batteryMode = BatteryModes.FREEZE  //discharging disabled
   }
 
   moveInDirection(dir, val) {
@@ -57,16 +60,17 @@ class Tag {
     }
 
     if (this.haltAtTarget) {
-      //squaring all calculations to prevent using root
-      let distance = (maxdistance.x * maxdistance.x + maxdistance.y * maxdistance.y)  
-      let margin = distance / 100 * 5 //5 percent error margin
+      //squaring all calculations to prevent using root (performance)
+      //let distanceSq = (maxdistance.x * maxdistance.x + maxdistance.y * maxdistance.y)  
+      let margin = 5//distanceSq / 100 * 5 //5 percent error margin, in case this provides inaccuracies change to constant value.
 
-      //untested, may and probably totally wont work
-      if (((this.targetLocation.x - this.position[x]) * (this.targetLocation.x - this.position[x]) //(X1 - X2)² + (Y1 - Y2)² = distance²
-          + (this.targetLocation.y - this.position[y]) * (this.targetLocation.y - this.position[y])) / distance < margin) {
+      //untested, should but may not work
+      //(X1 - X2)² + (Y1 - Y2)² = distance²
+      if (((this.targetLocation.x - this.position.x) * (this.targetLocation.x - this.position.x) ///(X1 - X2) * (X1 - X2) => (X1 - X2)²
+          + (this.targetLocation.y - this.position.y) * (this.targetLocation.y - this.position.y)) < (margin * margin)) {
           this.speed = 0
           return
-      } 
+      }
     }
 
     this.moveInDirection("x")
@@ -75,7 +79,7 @@ class Tag {
   }
 
   batteryTick(){
-   if (this.batteryMode = BatteryModes.FREEZE){
+   if (this.batteryMode == BatteryModes.FREEZE){
     return
    }
 
@@ -102,7 +106,7 @@ class Manager {
       const rand = speed * (factor / 3)
       const adjusted_speed = speed + (rand / 2) - (Math.random() * rand) // randomly +/- factor speed
       this.adjusted_speed *= interval / 5000 // take interval into account
-      this.tags.push(new Tag(tags[i].id, adjusted_speed))
+      this.tags.push(new Tag(tags[i], adjusted_speed))
     }
   }
 
@@ -117,11 +121,15 @@ class Manager {
     /**
       * (optional) teleportLocations: array of locations [x,y] to immediatly teleport tags to
       * (optional) targetLocations: array of locations [x,y] for the tags to move towards (after teleporting)
-      * (optional) haltAtTarget: set speed to zero when reached (near) targetLocation
+      * (optional) haltAtTarget: set speed to zero when reached/near targetLocation
+      * calling teleport without parameters (undef, undef, false) resumes tags at their original speed 
       */
     teleport(teleportLocations, targetLocations, haltAtTarget = false) { 
       let i = 0
       this.tags.forEach(function(tag) {
+
+        this.speed = this.originalSpeed
+
         if (teleportLocations[i] !== undefined) {
           tag.position = teleportLocations[i]
         } 
@@ -169,7 +177,7 @@ class Manager {
 
 const realtime = (server) => {
   const primus = new Primus(server, {})
-  const notifier = new Notifier() //TODO notifier integreren in live data
+  const notifier = new Notifier()
   notifier.initState(2) //INIT with map id 2
   notifier.initTriggers(2)
 
@@ -191,12 +199,14 @@ const realtime = (server) => {
     setInterval(() => {
       mgr.update()
 
-      const tagPositions = []
+      const tagData = []
 
       mgr.tags.forEach(function(tag) { 
-        const tagPosition = {
-          tagId: tag.tagId,
-          id: tag.tagId,
+        const tag = {
+         /* name: tag.name,
+          id: tag.id,
+          labels: tag.labels,
+          batteryLevel: tag.batteryLevel,
           position: {
             x: tag.position.x,
             y: tag.position.y,
@@ -207,12 +217,28 @@ const realtime = (server) => {
           y: tag.position.y,
           z: tag.position.z,
           timestamp: new Date().toISOString()
-
+*/
+          name: modelTag.name,
+          id: modelTag.id,
+          labels: modelTag.labels,
+          batteryLevel: modelTag.batteryLevel,
+          batteryMode: modelTag.batteryMode,
+          speed: modelTag.speed,
+          originalSpeed: modelTag.originalSpeed,
+          timestamp: new Date().toISOString(),
+          position: { 
+                      modelTag.position.x,
+                      modelTag.position.y,
+                      modelTag.position.y
+          }
         }
-        tagPositions.push(tagPosition)
+
+        tagData.push(tag)
+
       })
-      notifier.updateState({tags: tagPositions})
-      primus.write({action: 'SHOW_POSITIONS', positions: tagPositions})
+      notifier.updateState({tags: tagData})
+      //primus.write({action: 'SHOW_POSITIONS', positions: tagPositions}) 
+      primus.write({action: 'SEND_DATA', positions: tagData}) //not sure
     }, interval)
   })
 }
