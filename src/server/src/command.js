0229df4f7cd9bb5098fdbae99fae6b87bffb5632
Pieter-Jan Vandenberghe <pieterjan.vandenberghe@ugent.be>
2017-05-13 16:57:34 +0200

    Invention lvl 99 - Invented teleportation

--- src/server/src/command.js
+++ src/server/src/command.js
@@ -6,13 +6,13 @@ const command = (app, realtime) => {
     const scenario = req.body.scenario
     switch (req.body.type) {
       case "TELEPORT":
-        realtime.mgr.teleport(scenario)
+        realtime.mgr.teleport(scenario.teleportLocations, scenario.targetLocations, scenario.haltAtTarget)
         break
       case "BATTERY":
-        realtime.mgr.setBatteryMode(scenario)
+        realtime.mgr.setBatteryMode(scenario.modes)
         break
     }
-    res.status(200).json({success: true})
+    res.status(200).json(req.body)
   })
   app.use('/command', router)
 }
